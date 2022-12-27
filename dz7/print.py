ed204c6ebb7941d955a370bc75767cc50836b360
unknown <darya.sizova1998@yandex.ru>
2022-12-27 17:26:32 +0300

    принт

---
+++ dz7/print.py
@@ -0,0 +1,4 @@
+def print (data):
+    if len(data) > 0:
+        print("Фамилия".center(20), "Имя".center(20), "Отчество".center(20), "Телефон".center(15), "Категория".center(30))
+        print("-"*130)
